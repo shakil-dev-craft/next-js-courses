228fb76ef59959229b999e58bd0a83f5a4533d25
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-02 23:27:47 +0600

    Navigation and useRouter or Link learning done!

--- paglu-app/src/app/layout.js
+++ paglu-app/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* navlink */}
+        <div className="bg-gray-400">
+          <ul className="flex justify-center items-center gap-5 py-4">
+            <li className="text-gray-900 hover:text-white transition-all">
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li className="text-gray-900 hover:text-white transition-all">
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li className="text-gray-900 hover:text-white transition-all">
+              <Link href={"/userList"}>userList</Link>
+            </li>
+            <li className="text-gray-900 hover:text-white transition-all">
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+            <li className="text-gray-900 hover:text-white transition-all">
+              <Link href={"/signup"}>Signup</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* children */}
+        {children}
+      </body>
     </html>
   );
 }
--- paglu-app/src/app/page.js
+++ paglu-app/src/app/page.js
@@ -1,9 +1,18 @@
+"use client"
+
+import { useRouter } from "next/navigation";
 import State from "./State";
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <main>
       <State />
+
+      {/* btn */}
+      <button onClick={() => router.push('/userList')} type="button" className="py-2 px-5 bg-blue-700 text-white mt-5">Location Address</button>
     </main>
   );
 }
