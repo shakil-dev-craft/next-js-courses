f0145c611d5ca8173e1e3881940b1ea677b1762d
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-02 21:01:12 +0600

    not-found page add learning done!

---
+++ paglu-app/src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function notFound() {
+  return (
+    <div className='min-h-screen flex justify-center items-center w-full bg-gray-600'>
+        <h1 className='text-white'>Our Custom Made 404 Page || proccesing...</h1>
+    </div>
+  )
+}
+
+export default notFound
