93e3ba3382372eb4c754f82f7dbb845b81987d9e
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-02 21:16:13 +0600

    middleware create learning if i make a middleware then my new redirect login or other page and matcher redirect path url or path

---
+++ paglu-app/src/middleware.jsx
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+function middleware(request) {
+  return NextResponse.redirect(new URL('/registration', request.url))
+}
+
+export default middleware;
+
+export const config = {
+    matcher: '/class/:path*',
+};
