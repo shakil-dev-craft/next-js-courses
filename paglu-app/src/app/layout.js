9d5880c9b62a93deab7fafaa6eaa4839d29e4521
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-03 17:58:19 +0600

    Conditional show component

--- paglu-app/src/app/layout.js
+++ paglu-app/src/app/layout.js
@@ -16,23 +16,27 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* navlink */}
         <div className="bg-gray-400">
-          <ul className="flex justify-center items-center gap-5 py-4">
-            <li className="text-gray-900 hover:text-white transition-all">
-              <Link className={`${pathname === '/' ? 'active' : ''}`} href={"/"}>Home</Link>
-            </li>
-            <li className="text-gray-900 hover:text-white transition-all">
-              <Link className={`${pathname === '/about' ? 'active' : ''}`} href={"/about"}>About</Link>
-            </li>
-            <li className="text-gray-900 hover:text-white transition-all">
-              <Link className={`${pathname === '/userList' ? 'active' : ''}`} href={"/userList"}>userList</Link>
-            </li>
-            <li className="text-gray-900 hover:text-white transition-all">
-              <Link className={`${pathname === '/contact' ? 'active' : ''}`} href={"/contact"}>Contact</Link>
-            </li>
-            <li className="text-gray-900 hover:text-white transition-all">
-              <Link className={`${pathname === '/signup' ? 'active' : ''}`} href={"/signup"}>Signup</Link>
-            </li>
-          </ul>
+          {
+            pathname !== '/userList' ? 
+            <ul className="flex justify-center items-center gap-5 py-4">
+              <li className="text-gray-900 hover:text-white transition-all">
+                <Link className={`${pathname === '/' ? 'active' : ''}`} href={"/"}>Home</Link>
+              </li>
+              <li className="text-gray-900 hover:text-white transition-all">
+                <Link className={`${pathname === '/about' ? 'active' : ''}`} href={"/about"}>About</Link>
+              </li>
+              <li className="text-gray-900 hover:text-white transition-all">
+                <Link className={`${pathname === '/userList' ? 'active' : ''}`} href={"/userList"}>userList</Link>
+              </li>
+              <li className="text-gray-900 hover:text-white transition-all">
+                <Link className={`${pathname === '/product' ? 'active' : ''}`} href={"/product"}>Product</Link>
+              </li>
+              <li className="text-gray-900 hover:text-white transition-all">
+                <Link className={`${pathname === '/signup' ? 'active' : ''}`} href={"/signup"}>Signup</Link>
+              </li>
+            </ul> 
+            : null 
+          }
         </div>
 
         {/* children */}
